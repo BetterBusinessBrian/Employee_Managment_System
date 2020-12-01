8633b9011fce79db7661645882236a23aa3164d8
BetterBusinessBrian <bman1402@gmail.com>
2020-12-01 17:02:44 -0500

    added function for viewing employees by department and adding employees

--- seed.sql
+++ seed.sql
@@ -13,10 +13,15 @@ INSERT INTO roleTable
 salary,
 department_id)
 VALUES
-("Sales Lead", 50000, 1),
-("Engineer", 70000, 2),
+("Sales Lead", 70000, 1),
+("Account Manager", 45000, 1),
+("Lead Engineer", 80000, 2),
+("Engineer", 60000, 2),
 ("Accounts Payable", 45000, 3),
-("Lawyer", 80000, 4);
+("Accounts Receivable", 45000, 3),
+("Finance Manager", 80000, 3),
+("Lawyer", 80000, 4),
+("Manager", 75000, 4);
 
 INSERT INTO employees
 (first_name,
@@ -25,7 +30,13 @@ role_id,
 manager_id)
 VALUES
 ("Brian", "Whitman", 1, 1),
-("Alistair", "Rowden", 2, 2),
-("Bill", "Bryson", 3, 3);
+("Kren", "Witless", 2, 1),
+("Alistair", "Rowden", 3, 2),
+("Bob", "Builder", 4, 2),
+("Paul", "Payspeople", 5, 3),
+("Debbie", "Debtcollector", 6, 3),
+("Eugene", "Krabs", 7, 3),
+("Document", "McLegalson", 8, 4),
+("Bossy", "Inchargenson", 9);
 
 
--- tracker.js
+++ tracker.js
@@ -10,61 +10,143 @@ var connection = mysql.createConnection({
   database: "tracker_DB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
 });
 
 function start() {
   inquirer
-  .prompt({
-    name: "toDo",
-    type: "rawlist",
-    message: "What would you like to do?",
-    choices: [
-      "View All Employees",
-      "View All Employees By Department",
-      "View All Employees by Manager",
-      "Add Employee",
-      "Update Employee Role",
-      "Update Employee Manager"
-    ]
-  })
-  .then(function(answer) {
-    switch (answer.action) {
-    case "View All Employees":
-      // viewAllEmployees
-      break;
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "Add Employee",
+        "Update Employee Role",
+        "Update Employee Manager"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
 
-    case "View All Employees By Department":
-      // viewByDepartment
-      break;
+        case "View All Employees By Department":
+          viewByDepartment();
+          break;
 
-    case "View All Employees by Manager":
-      // viewByManager
-      break;
+        // case "View All Employees by Manager":
+        //  viewByManager();
+        //   break;
 
-    case "Add Employee":
-      // addEmployees
-      break;
+        case "Add Employee":
+          addEmployee();
+          break;
 
-    case "Update Employee Role":
-      // updateEmployeeRole
-      break;
+        // case "Update Employee Role":
+        //   // updateEmployeeRole
+        //   break;
 
-    case "Update Employee Manager":
-      // updateEmployeeManager
-      break;
-    }
-  });
+        // case "Update Employee Manager":
+        //   // updateEmployeeManager
+        //   break;
+      }
+    });
 }
 
 
-
-function viewAllEmployees(){
-  connection.query("select * from employees", function(err, res){
-    if(err) throw err;
+// View All
+function viewAllEmployees() {
+  var query = `select employees.id, employees.first_name, employees.last_name, roleTable.salary, roleTable.title, department.department_name, CONCAT(managers.first_name, ' ', managers.last_name) AS 'Manager'
+  FROM employees 
+  RIGHT JOIN roleTable 
+  ON employees.role_id = roleTable.id 
+  RIGHT JOIN department
+  ON roleTable.department_id = department.id
+  LEFT JOIN employees AS managers ON employees.manager_id = managers.id;`;
+  connection.query(query, function (err, res) {
+    if (err) throw err;
     console.table(res);
+    start();
   })
 }
 
-viewAllEmployees();
+// View by Department
+function viewByDepartment() {
+  inquirer
+    .prompt({
+      name: "departChoice",
+      type: "rawlist",
+      message: "Which Department would you like to see?",
+      choices: [
+        { name: "Sales", value: 1 },
+        { name: "Engineering", value: 2 },
+        { name: "Finance", value: 3 },
+        { name: "Legal", value: 4 },
+      ]
+    }).then(answers => {
+      var query = `SELECT employees.id, employees.first_name, employees.last_name, roleTable.salary, roleTable.title
+      FROM employees
+      RIGHT JOIN roleTable
+      ON employees.role_id = roleTable.id
+      WHERE roleTable.department_id = ?`;
+      connection.query(query, [answers.departChoice], function (err, res) {
+        if (err) throw err;
+        console.log('\n')
+        console.table(res)
+        console.log('\n')
+      }
+      )
+      start();
+    })
+}
+
+// View By Manager
+
+// Add an Employee
+async function addEmployee() {
+  const { firstName, lastName, roleid } = await inquirer.prompt([
+    {
+      name: "firstName",
+      message: "What is the new Employee's first name?",
+      type: "input"
+    },
+    {
+      name: "lastName",
+      message: "What is the new Employee's last name?",
+      type: "input"
+    },
+    {
+      name: "roleid",
+      message: "What is the new Employee's role?",
+      type: "rawlist",
+      choices: [
+        { name: "Sales Lead", value: 1 },
+        { name: "Account Manager", value: 2 },
+        { name: "Lead Engineer", value: 3 },
+        { name: "Engineer", value: 4 },
+        { name: "Accounts Payable", value: 5 },
+        { name: "Accounts Receivable", value: 6 },
+        { name: "Finance Manager", value: 7 },
+        { name: "Lawyer", value: 8 },
+        { name: "Manager", value: 9 }
+      ]
+    }])
+
+  connection.query(`INSERT INTO employees (first_name, last_name, role_id) VALUES ("${firstName}", "${lastName}", ${roleid})`, function (err, res) {
+    if (err) throw err;
+    console.log("Employee name: ", firstName, lastName, roleid);
+    start();
+  }
+  )
+
+}
+
+
+
+
+
+start();
